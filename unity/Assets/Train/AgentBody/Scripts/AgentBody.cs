16794ab5d8a275113055cce08e0faa94cd005ba0
DPC0516 <kyoungchan0516@gmail.com>
2026-02-06 21:13:15 +0900

    severed

--- unity/Assets/Train/AgentBody/Scripts/AgentBody.cs
+++ unity/Assets/Train/AgentBody/Scripts/AgentBody.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Train.AgentBody.Scripts {
     public class AgentBody : MonoBehaviour {
         public Quaternion RootStraightQuat { get; private set; }
         public JointConfig RootJointConfig { get; private set; }
+        public Dictionary<string, JointConfig> JointConfigDict { get; private set; }
 
 
         private void Awake() {
@@ -19,7 +21,8 @@ namespace Train.AgentBody.Scripts {
 
             RootStraightQuat = pelvis.transform.rotation;
 
-            RootJointConfig = JointConfig.BuildJointConfig(pelvis, null);
+            JointConfigDict = new Dictionary<string, JointConfig>();
+            RootJointConfig = BuildJointConfig(pelvis, null);
 
             string log = "Joint Structure:\n";
             LogJointConfig(RootJointConfig, 0, ref log);
@@ -34,5 +37,50 @@ namespace Train.AgentBody.Scripts {
                 LogJointConfig(child, depth + 1, ref log);
             }
         }
+
+        private JointConfig BuildJointConfig(ArticulationBody body, JointConfig parent) {
+            string bodyName = body.gameObject.name;
+
+            if (JointConfigDict.ContainsKey(bodyName)) {
+                throw new System.InvalidOperationException(
+                    $"Duplicate ArticulationBody name found: '{bodyName}'. " +
+                    "All ArticulationBody game objects must have unique names.");
+            }
+
+            JointConfig jointConfig = new() { ArticulationBody = body, LocalIsSevered = false, Parent = parent };
+
+            if (body.dofCount > 0) {
+                jointConfig.JointLimitCaches = new JointLimitCache[body.dofCount];
+                for (int i = 0; i < body.dofCount; i++) {
+                    ArticulationDrive drive = GetDriveForAxis(body, i);
+                    jointConfig.JointLimitCaches[i] = new JointLimitCache {
+                        LowerLimit = drive.lowerLimit * Mathf.Deg2Rad, UpperLimit = drive.upperLimit * Mathf.Deg2Rad
+                    };
+                }
+            }
+
+            JointConfigDict[bodyName] = jointConfig;
+
+            foreach (Transform childTransform in body.transform) {
+                ArticulationBody childBody = childTransform.GetComponent<ArticulationBody>();
+                if (!childBody) {
+                    continue;
+                }
+
+                JointConfig childConfig = BuildJointConfig(childBody, jointConfig);
+                jointConfig.Children.Add(childConfig);
+            }
+
+            return jointConfig;
+        }
+
+        private static ArticulationDrive GetDriveForAxis(ArticulationBody body, int axisIndex) =>
+            axisIndex switch {
+                0 => body.xDrive,
+                1 => body.yDrive,
+                2 => body.zDrive,
+                _ => throw new System.ArgumentOutOfRangeException(nameof(axisIndex),
+                    $"Invalid axis index {axisIndex}")
+            };
     }
 }
--- unity/Assets/Train/AgentBody/Scripts/JointConfig.cs
+++ unity/Assets/Train/AgentBody/Scripts/JointConfig.cs
@@ -9,44 +9,23 @@ namespace Train.AgentBody.Scripts {
 
     public class JointConfig {
         public ArticulationBody ArticulationBody;
-        public bool IsSevered;
+        public bool LocalIsSevered;
         public JointLimitCache[] JointLimitCaches;
         public readonly List<JointConfig> Children = new();
         public JointConfig Parent;
 
-        private ArticulationDrive GetDriveForAxis(int axisIndex) =>
-            axisIndex switch {
-                0 => ArticulationBody.xDrive,
-                1 => ArticulationBody.yDrive,
-                2 => ArticulationBody.zDrive,
-                _ => throw new System.ArgumentOutOfRangeException(nameof(axisIndex),
-                    $"Invalid axis index {axisIndex}")
-            };
-
-        public static JointConfig BuildJointConfig(ArticulationBody body, JointConfig parent) {
-            JointConfig jointConfig = new() { ArticulationBody = body, IsSevered = false, Parent = parent };
-
-            if (body.dofCount > 0) {
-                jointConfig.JointLimitCaches = new JointLimitCache[body.dofCount];
-                for (int i = 0; i < body.dofCount; i++) {
-                    ArticulationDrive drive = jointConfig.GetDriveForAxis(i);
-                    jointConfig.JointLimitCaches[i] = new JointLimitCache {
-                        LowerLimit = drive.lowerLimit * Mathf.Deg2Rad, UpperLimit = drive.upperLimit * Mathf.Deg2Rad
-                    };
-                }
-            }
-
-            foreach (Transform childTransform in body.transform) {
-                ArticulationBody childBody = childTransform.GetComponent<ArticulationBody>();
-                if (!childBody) {
-                    continue;
+        public bool IsSevered {
+            get {
+                if (LocalIsSevered) {
+                    return true;
                 }
 
-                JointConfig childConfig = BuildJointConfig(childBody, jointConfig);
-                jointConfig.Children.Add(childConfig);
-            }
+                if (Parent != null) {
+                    return Parent.IsSevered;
+                }
 
-            return jointConfig;
+                return false;
+            }
         }
     }
 }
