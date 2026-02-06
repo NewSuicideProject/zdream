3e0b865b128182513d2445b17c99b175d9df3d78
DPC0516 <kyoungchan0516@gmail.com>
2026-02-06 20:58:59 +0900

    zombie body restructured

---
+++ unity/Assets/AgentBody/Scripts/AgentBody.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace ZombieBody {
+    public class ZombieBody : MonoBehaviour {
+        public Quaternion RootStraightQuat { get; private set; }
+        public JointConfig RootJointConfig { get; private set; }
+
+
+        private void Awake() {
+            ArticulationBody pelvis = GetComponent<ArticulationBody>();
+            if (!pelvis) {
+                pelvis = GetComponentInChildren<ArticulationBody>();
+            }
+
+            if (!pelvis) {
+                Debug.LogError("No ArticulationBody found!");
+                return;
+            }
+
+            RootStraightQuat = pelvis.transform.rotation;
+
+            RootJointConfig = JointConfig.BuildJointConfig(pelvis, null);
+
+            string log = "Joint Structure:\n";
+            LogJointConfig(RootJointConfig, 0, ref log);
+            Debug.Log(log);
+        }
+
+        private static void LogJointConfig(JointConfig config, int depth, ref string log) {
+            string indent = new(' ', depth * 2);
+            log += $"{indent}{config.ArticulationBody.name} {config.ArticulationBody.dofCount}DoF\n";
+
+            foreach (JointConfig child in config.Children) {
+                LogJointConfig(child, depth + 1, ref log);
+            }
+        }
+    }
+}
---
+++ unity/Assets/AgentBody/Scripts/JointConfig.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieBody {
+    public struct JointLimitCache {
+        public float LowerLimit;
+        public float UpperLimit;
+    }
+
+    public class JointConfig {
+        public ArticulationBody ArticulationBody;
+        public bool IsSevered;
+        public JointLimitCache[] JointLimitCaches;
+        public readonly List<JointConfig> Children = new();
+        public JointConfig Parent;
+
+        private ArticulationDrive GetDriveForAxis(int axisIndex) =>
+            axisIndex switch {
+                0 => ArticulationBody.xDrive,
+                1 => ArticulationBody.yDrive,
+                2 => ArticulationBody.zDrive,
+                _ => throw new System.ArgumentOutOfRangeException(nameof(axisIndex),
+                    $"Invalid axis index {axisIndex}")
+            };
+
+        public static JointConfig BuildJointConfig(ArticulationBody body, JointConfig parent) {
+            JointConfig jointConfig = new() { ArticulationBody = body, IsSevered = false, Parent = parent };
+
+            if (body.dofCount > 0) {
+                jointConfig.JointLimitCaches = new JointLimitCache[body.dofCount];
+                for (int i = 0; i < body.dofCount; i++) {
+                    ArticulationDrive drive = jointConfig.GetDriveForAxis(i);
+                    jointConfig.JointLimitCaches[i] = new JointLimitCache {
+                        LowerLimit = drive.lowerLimit * Mathf.Deg2Rad, UpperLimit = drive.upperLimit * Mathf.Deg2Rad
+                    };
+                }
+            }
+
+            foreach (Transform childTransform in body.transform) {
+                ArticulationBody childBody = childTransform.GetComponent<ArticulationBody>();
+                if (!childBody) {
+                    continue;
+                }
+
+                JointConfig childConfig = BuildJointConfig(childBody, jointConfig);
+                jointConfig.Children.Add(childConfig);
+            }
+
+            return jointConfig;
+        }
+    }
+}
