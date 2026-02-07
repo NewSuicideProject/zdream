aa6bbd18615db3b4ad8c1b81012cf4a8c30946dc
DPC0516 <kyoungchan0516@gmail.com>
2026-02-07 22:51:17 +0900

    improvement

--- unity/Assets/Train/Navigation/Navigator.cs
+++ unity/Assets/Train/Navigation/Navigator.cs
@@ -6,52 +6,38 @@ using Unity.AI.Navigation;
 namespace Train.Navigation {
     public class Navigator : MonoBehaviour {
         [SerializeField] private Transform targetTransform;
+        [SerializeField] private Transform agentTransform;
         private NavMeshSurface _navMeshSurface;
-
-        private Vector3[] Corners { get; set; }
-        public Vector3[] GetCorners() => Corners ?? Array.Empty<Vector3>();
-
         private NavMeshPath _navMeshPath;
+
+        public Vector3[] Corners => _navMeshPath != null ? _navMeshPath.corners : Array.Empty<Vector3>();
 
         private void Awake() {
             _navMeshPath = new NavMeshPath();
             _navMeshSurface = GetComponent<NavMeshSurface>();
+            _navMeshSurface.BuildNavMesh();
         }
 
 
         private void Update() {
-            if (!targetTransform) {
+            if (!targetTransform && !agentTransform) {
                 return;
             }
 
-            Navigate();
-        }
-
-        private void Navigate() {
-            if (!_navMeshSurface) {
-                return;
-            }
-
-            _navMeshSurface.BuildNavMesh();
-
-            if (NavMesh.CalculatePath(transform.position, targetTransform.position, NavMesh.AllAreas, _navMeshPath)) {
-                Corners = _navMeshPath.corners;
-            } else {
-                Corners = Array.Empty<Vector3>();
-            }
+            NavMesh.CalculatePath(agentTransform.position, targetTransform.position, NavMesh.AllAreas, _navMeshPath);
         }
 
 
         private void OnDrawGizmos() {
-            if (Corners == null || Corners.Length < 2) {
+            if (Corners.Length == 0) {
                 return;
             }
 
             Gizmos.color = Color.red;
 
             for (int i = 0; i < Corners.Length - 1; i++) {
+                Gizmos.DrawSphere(Corners[i], 0.05f);
                 Gizmos.DrawLine(Corners[i], Corners[i + 1]);
-                Gizmos.DrawSphere(Corners[i], 0.1f);
             }
 
             Gizmos.DrawSphere(Corners[^1], 0.1f);
