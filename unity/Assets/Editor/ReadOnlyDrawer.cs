3586cc4e2c9057685d1bac11049c14a12a78aeaf
DPC0516 <kyoungchan0516@gmail.com>
2026-02-08 15:03:04 +0900

    adjustment

--- unity/Assets/Editor/ReadOnlyDrawer.cs
+++ unity/Assets/Editor/ReadOnlyDrawer.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        GUI.enabled = false;
+namespace Editor {
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            GUI.enabled = false;
 
-        EditorGUI.PropertyField(position, property, label);
+            EditorGUI.PropertyField(position, property, label);
 
-        GUI.enabled = true;
+            GUI.enabled = true;
+        }
     }
 }
