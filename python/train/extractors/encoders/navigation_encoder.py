46b5e91a9deb17642830e6eea3df3f966eed5b62
gastonia-86 <kdino1972@gmail.com>
2026-02-09 02:11:43 +0900

    updated basic nav-encoder

--- python/train/extractors/encoders/navigation_encoder.py
+++ python/train/extractors/encoders/navigation_encoder.py
@@ -1,30 +1,42 @@
+import torch as th
 import torch.nn as nn
-
 
 class NavigationEncoder(nn.Module):
     def __init__(
-        self,
-        input_dim,
-        hidden_dims=None,
-        activation_fn=None,
+        self, 
+        input_dim=5, 
+        d_model=128, 
+        nhead=2, 
+        num_layers=2, 
+        max_t=10
     ):
         super().__init__()
-
-        if hidden_dims is None:
-            hidden_dims = [64, 128, 256]
-        if activation_fn is None:
-            activation_fn = nn.ReLU
-
-        layers = []
-        last_dim = input_dim
-
-        for hidden_dim in hidden_dims:
-            layers.append(nn.Linear(last_dim, hidden_dim))
-            layers.append(activation_fn())
-            last_dim = hidden_dim
-
-        self.net = nn.Sequential(*layers)
-        self.output_dim = last_dim
+        
+        assert d_model % nhead == 0
+        
+        self.input_projection = nn.Linear(input_dim, d_model)
+        self.pos_embedding = nn.Parameter(th.zeros(1, max_t, d_model))
+        
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dim_feedforward=d_model * 4,
+            batch_first=True,
+            activation="gelu"
+        )
+        
+        self.transformer_blocks = nn.TransformerEncoder(
+            encoder_layer, 
+            num_layers=num_layers
+        )
+        
+        self.output_dim = d_model
 
     def forward(self, x):
-        return self.net(x)
+        x = self.input_projection(x)
+        
+        x = x + self.pos_embedding[:, :x.size(1), :]
+        
+        x = self.transformer_blocks(x)
+        
+        return x.mean(dim=1)
