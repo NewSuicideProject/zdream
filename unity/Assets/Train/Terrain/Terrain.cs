1d7ac11064e7cd92a48795501c74b9ad276ea153
DPC0516 <kyoungchan0516@gmail.com>
2026-02-12 16:31:43 +0900

    adjustment

--- unity/Assets/Train/Terrain/Terrain.cs
+++ unity/Assets/Train/Terrain/Terrain.cs
@@ -2,59 +2,61 @@ using UnityEngine;
 
 namespace Train.Terrain {
     public class Terrain : MonoBehaviour {
-        [Header("Settings")] [SerializeField] private float heightOffset = 10f;
+        [SerializeField] private float heightOffset = 10f;
         [SerializeField] private int gridSize = 10;
         [SerializeField] private float spacing = 1f;
         [SerializeField] private LayerMask targetLayer;
-        [SerializeField] private Proprioception proprioception;
+        private Proprioception _proprioception;
 
-        private Vector3 _centerPosition;
+        private Vector3 _position;
+
         public float[,] HeightMap;
-        private void Awake() => HeightMap = new float[gridSize, gridSize];
 
-        private void Update() => ScanHeightMap();
+        private void Awake() {
+            _proprioception = GetComponentInChildren<Proprioception>();
+            HeightMap = new float[gridSize, gridSize];
+        }
 
-        private void ScanHeightMap() {
-            _centerPosition = !proprioception ? transform.position : proprioception.Position;
+        private void Update() {
+            _position = _proprioception?.Position ?? transform.position;
 
-            float halfSize = (gridSize - 1) * spacing * 0.5f;
-            Vector3 startPos = new(_centerPosition.x - halfSize,
-                _centerPosition.y + heightOffset, _centerPosition.z + halfSize);
+            float gridHalfSize = (gridSize - 1) * spacing * 0.5f;
+            Vector3 gridTopLeft = new(_position.x - gridHalfSize,
+                _position.y + heightOffset, _position.z + gridHalfSize);
 
             for (int z = 0; z < gridSize; z++) {
                 for (int x = 0; x < gridSize; x++) {
-                    Vector3 rayOrigin = startPos + new Vector3(x * spacing, 0, -z * spacing);
+                    Vector3 rayOrigin = gridTopLeft + new Vector3(x * spacing, 0, -z * spacing);
 
                     if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit,
                             heightOffset * 2f, targetLayer)) {
                         HeightMap[x, z] = hit.point.y;
                     } else {
-                        HeightMap[x, z] = _centerPosition.y - heightOffset;
+                        HeightMap[x, z] = 0f;
                     }
                 }
             }
         }
 
         private void OnDrawGizmos() {
-            if (HeightMap == null || HeightMap.GetLength(0) != gridSize) {
+            if (HeightMap == null) {
                 return;
             }
 
-            Gizmos.color = Color.red;
-            float halfSize = (gridSize - 1) * spacing * 0.5f;
-
-            Vector3 currentCenter = Application.isPlaying ? _centerPosition : transform.position;
-            Vector3 startPos = new(currentCenter.x - halfSize,
-                currentCenter.y + heightOffset, currentCenter.z + halfSize);
+            float gridHalfSize = (gridSize - 1) * spacing * 0.5f;
+            Vector3 gridTopLeft = new(_position.x - gridHalfSize,
+                _position.y + heightOffset, _position.z + gridHalfSize);
 
             for (int z = 0; z < gridSize; z++) {
                 for (int x = 0; x < gridSize; x++) {
-                    Vector3 rayOrigin = startPos + new Vector3(x * spacing, HeightMap[x, z], -z * spacing);
+                    Vector3 rayOrigin = gridTopLeft + new Vector3(x * spacing, 0, -z * spacing);
                     Vector3 hitPoint = new(rayOrigin.x, HeightMap[x, z], rayOrigin.z);
 
-                    Gizmos.DrawLine(rayOrigin, hitPoint);
-                    Gizmos.DrawSphere(hitPoint, 0.05f);
-                    Gizmos.DrawSphere(rayOrigin, 0.05f);
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawSphere(hitPoint, 0.025f);
+
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(rayOrigin, 0.025f);
                 }
             }
         }
