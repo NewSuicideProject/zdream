6db50474120d0316133bf44cf81fa780ff092601
DPC0516 <kyoungchan0516@gmail.com>
2026-02-22 02:52:45 +0900

    1

---
+++ unity/Assets/Scripts/Train/Joint/Force.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Train.Joint {
+    public class ForceReceiver : MonoBehaviour {
+        public Vector3 Force { get; private set; }
+
+        private void OnCollisionStay(Collision collision) {
+            Vector3 accumulated = Vector3.zero;
+            foreach (ContactPoint contact in collision.contacts) {
+                accumulated += contact.normal * contact.impulse.magnitude;
+            }
+            Force += accumulated;
+        }
+
+        private void FixedUpdate() => Force = Vector3.zero;
+    }
+}
+
