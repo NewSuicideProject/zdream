90fb8b095c5b0f0411de91432fd8711d61b5ba23
DPC0516 <kyoungchan0516@gmail.com>
2026-02-25 10:36:34 +0900

    refactoring

--- python/train/run.py
+++ python/train/run.py
@@ -1,13 +1,11 @@
-import importlib
 import logging
 import os
-import re
 from functools import partial
 from pathlib import Path
 
 import hydra
 from dotenv import load_dotenv
-from hydra.utils import get_original_cwd
+from hydra.utils import get_class, get_original_cwd
 from omegaconf import DictConfig, OmegaConf
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.monitor import Monitor
@@ -26,7 +24,7 @@ def make_unity_env(file_name: str, worker_id: int, env_params: dict):
     return UnityEnv(unity_path=file_name, worker_id=worker_id, unity_kwargs=env_params)
 
 
-def validate_path(path_str):
+def get_path(path_str):
     if not path_str:
         return None
     path = Path(path_str)
@@ -38,63 +36,48 @@ def validate_path(path_str):
     return path
 
 
+def get_dict_config(cfg: DictConfig, path: str) -> DictConfig:
+    node = OmegaConf.select(cfg, path)
+    return node if node is not None else OmegaConf.create({})
+
+
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def run(config: DictConfig):
     logger.info(f"config: \n{OmegaConf.to_yaml(config)}")
 
     base_dir = Path.cwd()
     log_dir = base_dir / "log"
-    model_path = base_dir / "result.zip"
+    model_path = base_dir / "model.zip"
     checkpoint_dir = base_dir / "checkpoints"
 
-    unity_server_path = validate_path(os.getenv("UNITY_SERVER_PATH", None))
-    unity_path = validate_path(os.getenv("UNITY_PATH", None))
+    unity_server_path = get_path(os.getenv("UNITY_SERVER_PATH", None))
+    unity_path = get_path(os.getenv("UNITY_PATH", None))
     if not unity_path:
         unity_path = unity_server_path
 
-    checkpoint_path = validate_path(os.getenv("CHECKPOINT_PATH", None))
+    checkpoint_path = get_path(os.getenv("CHECKPOINT_PATH", None))
 
-    # Merge kwargs from different config groups
-    policy_kwargs = {}
-    if "model" in config and "policy_kwargs" in config.model:
-        policy_kwargs.update(
-            OmegaConf.to_container(config.model.policy_kwargs, resolve=True)
-        )
-    if "curriculum" in config and "policy_kwargs" in config.curriculum:
-        curr_policy = OmegaConf.to_container(
-            config.curriculum.policy_kwargs, resolve=True
-        )
-        for k, v in curr_policy.items():
-            if (
-                isinstance(v, dict)
-                and k in policy_kwargs
-                and isinstance(policy_kwargs[k], dict)
-            ):
-                policy_kwargs[k].update(v)
-            else:
-                policy_kwargs[k] = v
+    policy_kwargs: dict = OmegaConf.to_container(
+        OmegaConf.merge(
+            get_dict_config(config, "model.policy_kwargs"),
+            get_dict_config(config, "curriculum.policy_kwargs"),
+        ),
+        resolve=True,
+    )
 
-    unity_kwargs = {}
-    if "model" in config and "unity_kwargs" in config.model:
-        unity_kwargs.update(
-            OmegaConf.to_container(config.model.unity_kwargs, resolve=True)
-        )
-    if "normalization" in config and "unity_kwargs" in config.normalization:
-        unity_kwargs.update(
-            OmegaConf.to_container(config.normalization.unity_kwargs, resolve=True)
-        )
-    if "curriculum" in config and "unity_kwargs" in config.curriculum:
-        unity_kwargs.update(
-            OmegaConf.to_container(config.curriculum.unity_kwargs, resolve=True)
-        )
+    unity_kwargs: dict = OmegaConf.to_container(
+        OmegaConf.merge(
+            get_dict_config(config, "model.unity_kwargs"),
+            get_dict_config(config, "normalization.unity_kwargs"),
+            get_dict_config(config, "curriculum.unity_kwargs"),
+        ),
+        resolve=True,
+    )
 
     if "features_extractor_class" in policy_kwargs:
-        class_name = policy_kwargs["features_extractor_class"]
-        if isinstance(class_name, str):
-            module_name = re.sub(r"(?<!^)(?=[A-Z])", "_", class_name).lower()
-            module_path = f".extractors.{module_name}"
-            module = importlib.import_module(module_path, package=__package__)
-            policy_kwargs["features_extractor_class"] = getattr(module, class_name)
+        policy_kwargs["features_extractor_class"] = get_class(
+            policy_kwargs["features_extractor_class"]
+        )
 
     train_cfg = config.train
 
